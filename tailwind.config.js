a6e839f1ede48fb84d4f81e9ebbef92272e2f9f4
leson14082002 <145142490+leson14082002@users.noreply.github.com>
2025-03-28 10:54:23 +0700

    Add tailwind.config.js 1743134064725

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'tesla-red': '#cc0000',
+      },
+      fontFamily: {
+        sans: ['Gotham SSm', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
